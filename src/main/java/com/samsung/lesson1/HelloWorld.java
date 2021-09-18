85ddf83fba03db3ee586c84edadf22b8aa476f5a
Михаил Левин <mikelevin@yandex-team.ru>
2021-09-18 18:36:54 +0300

    New code Lesson 1.

--- src/main/java/com/samsung/lesson1/HelloWorld.java
+++ src/main/java/com/samsung/lesson1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // sout
         System.out.println("Hello world");
+        System.out.println("Next comment");
     }
 }
