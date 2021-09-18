be3f4c2948f254e6e45e03b9b0af197e6cf7bddc
Михаил Левин <mikelevin@yandex-team.ru>
2021-09-18 18:34:25 +0300

    Samsung It School Lesson 1. Data types

---
+++ src/main/java/com/samsung/lesson1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.samsung.lesson1;
+
+public class HelloWorld {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello world");
+    }
+}
